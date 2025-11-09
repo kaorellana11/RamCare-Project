272492a3a755b73835842e91057b59b37c195467
Kevin Orellana <kaorellana52@gmail.com>
2025-11-09 14:43:24 -0500

    Formatted Log In Screen & added .gitignore
    Widgets are now properly spaced out from each other

--- Sprint 2/login_frontend.py
+++ Sprint 2/login_frontend.py
@@ -3,42 +3,47 @@ import customtkinter as ctk
 FONT_SIZE = 20
 FONT_FAMILY = "Lucida Console"
 
-def setFont(element):
-    element.cget("font").configure(size=FONT_SIZE, family=FONT_FAMILY)
+def setFont(element, size=FONT_SIZE, family=FONT_FAMILY):
+    element.cget("font").configure(size=size, family=family)
 
 class LogInScreen(ctk.CTkFrame):
     #note: you need 8 rows total
     def __init__(self, master):
         super().__init__(master)
         self.grid_columnconfigure(0, weight=1)
-        #self.grid_rowconfigure(1, weight=1)
-        #self.configure("#E9E9E9")
-        top_label = ctk.CTkLabel(self, text= "Log In")
+
+        top_label = ctk.CTkLabel(self, text= "Log In", fg_color="transparent", bg_color="#3b8ed0")
         top_label.grid(row = 0, column = 0, sticky="ew")
         setFont(top_label)
 
+        border = ctk.CTkFrame(self, height=2, fg_color="black")
+        border.grid(row=1, column=0, sticky="ew")
+
         username_label = ctk.CTkLabel(self, text="Username")
-        username_label.grid(row=1, column=0)
+        username_label.grid(row=2, column=0, pady=(75, 0))
         setFont(username_label)
 
         self.username_input = ctk.CTkTextbox(self, height=35, border_color="black", border_width=2)
-        self.username_input.grid(row=2, column=0)
+        self.username_input.grid(row=3, column=0)
 
         password_label = ctk.CTkLabel(self, text="Password")
-        password_label.grid(row=3, column=0)
+        password_label.grid(row=4, column=0, pady=(100, 0))
         setFont(password_label)
 
         self.password_input = ctk.CTkTextbox(self, height=35, border_color="black", border_width=2)
-        self.password_input.grid(row=4, column=0)
+        self.password_input.grid(row=5, column=0)
 
         forgot_password_button = ctk.CTkButton(self, text="Forgot Password?", text_color="blue", fg_color="transparent",hover=False, command=self.forgot_password)
-        forgot_password_button.grid(row=5, column=0)
+        forgot_password_button.grid(row=6, column=0, pady=(10, 0))
+        setFont(forgot_password_button, size=14)
 
         self.remember_check = ctk.CTkCheckBox(self ,text="Remember Me")
-        self.remember_check.grid(row=6, column=0)
+        self.remember_check.grid(row=7, column=0, pady=(10, 0))
+        setFont(self.remember_check, size=14)
 
         self.log_in_button = ctk.CTkButton(self, text="Log In", border_width=2, border_color="black", corner_radius=10)
-        self.log_in_button.grid(row=7, column=0)
+        self.log_in_button.grid(row=8, column=0, pady=(10, 0))
+        setFont(self.log_in_button, size=14)
 
     def forgot_password(self):
         pass
