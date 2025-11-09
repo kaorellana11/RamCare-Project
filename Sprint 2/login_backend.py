7ca06e8be6606ec565f7ae5a4835779776a514d4
Robert Smith <robert.e.smith1020@gmail.com>
2025-11-09 16:26:10 -0500

    Updated login_backend
    is_pass will now return False if given a bad username. Updated the table to now store security questions as well.

--- Sprint 2/login_backend.py
+++ Sprint 2/login_backend.py
@@ -11,15 +11,18 @@ def create_table():
         CREATE TABLE IF NOT EXISTS Login (
             Username VARCHAR(30) NOT NULL,
             Password VARCHAR(80) NOT NULL,
-            Salt VARCHAR(50) NOT NULL
+            Salt VARCHAR(50) NOT NULL,
+            SecQ1 VARCHAR(100) NOT NULL,
+            SecQ2 VARCHAR(100) NOT NULL,
+            SecQ3 VARCHAR(100) NOT NULL
         );
     ''')
 
-def store_login(username, password):
+def store_login(username, password, secq1, secq2, secq3):
     encoded = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashedPass = bcrypt.hashpw(encoded, salt)
-    cursor.execute("INSERT INTO Login (Username, Password, Salt) VALUES (?, ?, ?)", (username, hashedPass, salt))
+    cursor.execute("INSERT INTO Login (Username, Password, Salt, SecQ1, SecQ2, SecQ3) VALUES (?, ?, ?, ?, ?, ?)", (username, hashedPass, salt, secq1, secq2, secq3))
     #sort_user() // Don't know if needed yet
 
     connection_obj.commit()
@@ -29,16 +32,20 @@ def sort_user():
         CREATE TABLE Login_ordered (
             Username VARCHAR(30) NOT NULL,
             Password VARCHAR(50) NOT NULL
+            Salt VARCHAR(50) NOT NULL,
+            SecQ1 VARCHAR(100) NOT NULL,
+            SecQ2 VARCHAR(100) NOT NULL,
+            SecQ3 VARCHAR(100) NOT NULL
         );
     ''')
-    cursor.execute("INSERT INTO Login_ordered (Username, Password) SELECT Username, Password FROM Login ORDER BY Username;")
+    cursor.execute("INSERT INTO Login_ordered (Username, Password, Salt, SecQ1, SecQ2, SecQ3) SELECT Username, Password, Salt, SecQ1, SecQ2, SecQ3 FROM Login ORDER BY Username;")
 
 def search_pass(username):
     cursor.execute("SELECT Password FROM Login WHERE Username = ?", (username,))
-    coolpassword = cursor.fetchone()
-    coolpassword = coolpassword[-1]
+    foundpass = cursor.fetchone()
+    foundpass = foundpass[-1]
 
-    return coolpassword
+    return foundpass
 
 def is_password(username, password):
     storedPass = search_pass(username)
@@ -46,6 +53,9 @@ def is_password(username, password):
     encoded = password.encode('utf-8')
 
     packed_salt = cursor.fetchone()
+    if len(packed_salt) == 0:
+        return False
+    
     salt = packed_salt[-1]
 
     hashedPass = bcrypt.hashpw(encoded, salt)
