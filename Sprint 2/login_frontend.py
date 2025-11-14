9c7a2b4e6170e0fa0807e0b9d646ee6451260547
Kevin Orellana <kaorellana52@gmail.com>
2025-11-13 19:20:13 -0500

    Implemented screen to indicate user is logged in

--- Sprint 2/login_frontend.py
+++ Sprint 2/login_frontend.py
@@ -7,8 +7,31 @@ FONT_FAMILY = "Lucida Console"
 def setFont(element, size=FONT_SIZE, family=FONT_FAMILY):
     element.cget("font").configure(size=size, family=family)
 
+class LoggedInScreen(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.master = master
+
+        top_label = ctk.CTkLabel(self, text= "Logged In", fg_color="transparent", bg_color="#3b8ed0")
+        top_label.grid(row = 0, column = 0, sticky="ew")
+        setFont(top_label)
+
+        border = ctk.CTkFrame(self, height=2, fg_color="black")
+        border.grid(row=1, column=0, sticky="ew")
+        
+        self.info_label = ctk.CTkLabel(self, text="test", fg_color="transparent")
+        self.info_label.grid(row=2, column=0, pady=(75, 0))
+        setFont(self.info_label)
+
+        self.log_out_button = ctk.CTkButton(self, text="Log Out", border_width=2, border_color="black", command=self.log_out)
+        self.log_out_button.grid(row=3, column=0, pady=(75, 0))
+        setFont(self.log_out_button)
+    
+    def log_out(self):
+        self.master.log_in_screen.tkraise()
+
 class LogInScreen(ctk.CTkFrame):
-    #note: you need 8 rows total
     def __init__(self, master):
         super().__init__(master)
         self.grid_columnconfigure(0, weight=1)
@@ -65,16 +88,20 @@ class LogInScreen(ctk.CTkFrame):
             
         if back.is_password(*credentials):
             self.wrong_password_label.grid_remove()
+            self.master.security_screen.user_name = credentials[0]
             self.master.security_screen.tkraise()
+
+            for box in self.input_boxes:
+                box.delete("0.0", "end-1c")
         else:
             self.wrong_password_label.grid()
-
 
 class SecurityQuestionScreen(ctk.CTkFrame):
     def __init__(self, master, questions):
         super().__init__(master)
         self.master = master
         self.grid_columnconfigure(0, weight=1)
+        self.user_name = "" #to be filled by LogInScreen
         top_label = ctk.CTkLabel(self, text= "Log In", fg_color="transparent", bg_color="#3b8ed0")
         top_label.grid(row = 0, column = 0, sticky="ew")
         setFont(top_label)
@@ -93,11 +120,29 @@ class SecurityQuestionScreen(ctk.CTkFrame):
             question_box.grid(row=cur_row, column=0, pady=(0, 50))
             self.input_boxes.append(question_box)
             cur_row += 1
-        self.log_in_button = ctk.CTkButton(self, text="Log In", border_width=2, border_color="black", corner_radius=10)
+        self.log_in_button = ctk.CTkButton(self, text="Log In", border_width=2, border_color="black", corner_radius=10, command=self.log_in)
         self.log_in_button.grid(row=cur_row, column=0, pady=(10, 0))
         setFont(self.log_in_button, size=14)
-    
+        cur_row += 1
 
+        self.wrong_password_label = ctk.CTkLabel(self, text="Incorrect Username or Password!", text_color="red")
+        self.wrong_password_label.grid(row=cur_row, column=0, pady=(10, 0))
+        setFont(self.wrong_password_label)
+        self.wrong_password_label.grid_remove()
+
+    def log_in(self):
+        sec_params = [self.user_name]
+        for box in self.input_boxes:
+            sec_params.append(box.get("0.0", "end-1c"))
+        if back.is_security(*sec_params):
+            self.wrong_password_label.grid_remove()
+            self.master.logged_in_screen.info_label.configure(text="Logged in as " + str(self.user_name) + ".")
+            for box in self.input_boxes:
+                box.delete("0.0", "end-1c")
+            self.master.logged_in_screen.tkraise()
+        else:
+            self.wrong_password_label.grid()
+    
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -107,6 +152,9 @@ class App(ctk.CTk):
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
+
+        self.logged_in_screen = LoggedInScreen(self)
+        self.logged_in_screen.grid(row=0, column=0, sticky="nsew")
 
         self.security_screen = SecurityQuestionScreen(self, ["What city were you born in?", "What is your aunt's middle name?", "Where did you first attend school?"])
         self.security_screen.grid(row=0, column=0, sticky="nsew")
