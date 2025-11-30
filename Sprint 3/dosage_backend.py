e98ad9a874f34112046d47d4c1d36a7b029b0f2d
Robert Smith <robert.e.smith1020@gmail.com>
2025-11-30 18:56:05 -0500

    Updated dosage_backend.py
    Added the Dosage flag to the table. Updated store_login to reflect this. In addition, added the get_dob, confirm_dosage, and next_day functions.

--- Sprint 3/dosage_backend.py
+++ Sprint 3/dosage_backend.py
@@ -20,11 +20,12 @@ def create_table():
             SaltQ3 VARCHAR(50) NOT NULL,
             Dob CHAR(10) NOT NULL,
             Provider VARCHAR(100) NOT NULL,
-            Medication VARCHAR(100) NOT NULL
+            Medication VARCHAR(100) NOT NULL,
+            Dosage INTEGER NOT NULL
         );
     ''')
 
-def store_login(username, password, secq1, secq2, secq3, dob, provider, medications):
+def store_login(username, password, secq1, secq2, secq3, dob, provider, medications, dosage):
     encodedPass = password.encode('utf-8')
     passSalt = bcrypt.gensalt()
     hashedPass = bcrypt.hashpw(encodedPass, passSalt)
@@ -41,9 +42,18 @@ def store_login(username, password, secq1, secq2, secq3, dob, provider, medicati
     saltQ3 = bcrypt.gensalt()
     hashedQ3 = bcrypt.hashpw(encodedSecQ3, saltQ3)
 
-    cursor.execute("INSERT INTO Patients (Username, Password, PassSalt, SecQ1, SaltQ1, SecQ2, SaltQ2, SecQ3, SaltQ3, Dob, Provider, Medication) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (username, hashedPass, passSalt, hashedQ1, saltQ1, hashedQ2, saltQ2, hashedQ3, saltQ3, dob, provider, medications))
+    cursor.execute("INSERT INTO Patients (Username, Password, PassSalt, SecQ1, SaltQ1, SecQ2, SaltQ2, SecQ3, SaltQ3, Dob, Provider, Medication, Dosage) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (username, hashedPass, passSalt, hashedQ1, saltQ1, hashedQ2, saltQ2, hashedQ3, saltQ3, dob, provider, medications, dosage))
 
     connection_obj.commit()
+
+def get_dob(username):
+    cursor.execute("SELECT Dob FROM Patients WHERE Username = ?", (username,))
+    foundDob = cursor.fetchone()
+    if foundDob is None:
+        return False
+    foundDob = foundDob[-1]
+
+    return foundDob
 
 def get_medications(username):
     cursor.execute("SELECT Medication FROM Patients WHERE Username = ?", (username,))
@@ -51,6 +61,10 @@ def get_medications(username):
     if foundMeds is None:
         return False
     foundMeds = foundMeds[-1]
+
+    character = ','
+    if character in foundMeds:
+        foundMeds = foundMeds.split(',')
 
     return foundMeds
 
@@ -63,6 +77,16 @@ def get_provider(username):
 
     return foundProv
 
+def confirm_dosage(username):
+    cursor.execute("UPDATE Patients SET Dosage = True WHERE Username = ?", (username,))
+    connection_obj.commit()
+    print("Patient Dosage Taken Confirmed")
+
+def next_day():
+    cursor.execute("UPDATE Patients SET Dosage = False")
+    connection_obj.commit()
+    print("Patients Confirmation Reset")
+
 def main():
     create_table()
 
@@ -74,8 +98,9 @@ def main():
     dob = "01/01/1999"
     provider = "Dominion Medical Associates"
     medications = "Tylenol"
+    dosage = False
 
-    store_login(username, password, q1, q2, q3, dob, provider, medications)
+    store_login(username, password, q1, q2, q3, dob, provider, medications, dosage)
 
     testProv = get_provider(username)
     if (testProv == provider):
@@ -84,6 +109,9 @@ def main():
     testMeds = get_medications(username)
     if (testMeds == medications):
         print("Get Medications works")
+    
+    confirm_dosage(username)
+    next_day()
     
 if __name__ == "__main__":
     main()
